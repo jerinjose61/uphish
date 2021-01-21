e0b2d87e7fb14c997021673431e1f0d1bb4cb608
Jerin Jose <jerinjose61@gmail.com>
2021-01-21 17:16:28 +0530

    added first_name & last_name

--- app/tasks.py
+++ app/tasks.py
@@ -43,7 +43,9 @@ def launch_campaign(campaign_name, from_email, target_group, phishing_page, emai
                         'host':host,
                         'phishing_url':phishing_url,
                         'campaign_id':campaign_id,
-                        'target_id':target_id})
+                        'target_id':target_id,
+                        'first_name':target.first_name,
+                        'last_name':target.last_name})
 
         try:
             email = EmailMessage(subject = subject, body = html_message, from_email = from_email,
